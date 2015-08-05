215302872ba5ef1b555e018c5872135b926b555a
Rory MacQueen <rorymacqueen@gmail.com>
2015-08-06 01:56:03 +0300

    Make ball bounce around screen

--- ball.py
+++ ball.py
@@ -8,4 +8,20 @@ palgame.build_screen(SCREEN_WIDTH, SCREEN_HEIGHT)
 x = SCREEN_WIDTH / 2
 y = SCREEN_HEIGHT / 2
 
-palgame.draw_circle(x, y, 10)
+xdir = 1
+ydir = 1
+
+while True:
+	palgame.get_event()
+	palgame.clear_screen()
+	palgame.draw_circle(x, y, 10)
+	if x > SCREEN_WIDTH:
+		xdir = -1
+	if x < 0:
+		xdir = 1
+	if y > SCREEN_HEIGHT:
+		ydir = -1
+	if y < 0:
+		ydir = 1
+	x = x + xdir * 10
+	y = y + ydir * 10
