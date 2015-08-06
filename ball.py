522499f27730da63ed681c8a793326f61b4c23f2
Rory MacQueen <rorymacqueen@gmail.com>
2015-08-06 18:02:41 +0300

    Starter code for agar

--- ball.py
+++ ball.py
@@ -1,7 +1,13 @@
 import palgame
 
-SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 700
+NUM_BALLS = 5
+NUM_FOOD = 20
+
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
 
 palgame.build_screen(SCREEN_WIDTH, SCREEN_HEIGHT)
 
@@ -10,18 +16,73 @@ y = SCREEN_HEIGHT / 2
 
 xdir = 1
 ydir = 1
+balls = []
+food = []
+
+
+for a in range(NUM_BALLS):
+	balls.append(palgame.get_random_ball())
+
+for c in range(NUM_FOOD):
+	food.append(palgame.get_random_food())
+
+def update_ball_position(ball, speed):
+	x, y, radius, xdir, ydir = ball
+
+	if x > SCREEN_WIDTH or x < 0:
+		xdir = -1 * xdir
+	if y > SCREEN_HEIGHT or y < 0:
+		ydir = -1 * ydir
+	x = x + xdir * speed
+	y = y + ydir * speed
+	return [x, y, radius, xdir, ydir]
+
+def balls_collide(ball1, ball2):
+	if ( abs(palgame.get_x(ball2) - palgame.get_x(ball1)) < (palgame.get_radius(ball2) + palgame.get_radius(ball1))
+		and abs(palgame.get_y(ball2) - palgame.get_y(ball1)) < (palgame.get_radius(ball2) + palgame.get_radius(ball1)) ):
+		return True
+	else:
+		return False
 
 while True:
 	palgame.get_event()
+	for a in range(NUM_BALLS):
+		ball = balls[a]
+		speed = palgame.get_speed(palgame.get_radius(ball))
+		if a == 0:
+			mx, my = palgame.get_mouse_location()
+			new_x = palgame.get_x(ball) + int(palgame.get_vx(speed, mx, my, palgame.get_x(ball), palgame.get_y(ball)))
+			palgame.set_x(ball, new_x)
+			new_y = palgame.get_y(ball) + int(palgame.get_vy(speed, mx, my, palgame.get_x(ball), palgame.get_y(ball)))
+			palgame.set_y(ball, new_y)
+		else:
+			ball = update_ball_position(ball, speed)
+
+		for f in range(NUM_FOOD):
+			nom = food[f]
+			if balls_collide(ball, nom):
+				new_radius = palgame.get_radius(ball) + (1 - palgame.get_radius(ball) / 100)
+				palgame.set_radius(ball, new_radius)
+				food[f] = palgame.get_random_food()
+
+		for b in range(NUM_BALLS):
+			other_ball = balls[b]
+			if a != b and balls_collide(ball, other_ball):
+				radius = palgame.get_radius(ball)
+				if radius >= palgame.get_radius(other_ball):
+					new_radius = radius + (1 - radius / 100)
+					palgame.set_radius(ball, new_radius)
+					balls[b] = palgame.get_random_ball()
+
+		balls[a] = ball
 	palgame.clear_screen()
-	palgame.draw_circle(x, y, 10)
-	if x > SCREEN_WIDTH:
-		xdir = -1
-	if x < 0:
-		xdir = 1
-	if y > SCREEN_HEIGHT:
-		ydir = -1
-	if y < 0:
-		ydir = 1
-	x = x + xdir * 10
-	y = y + ydir * 10
+	for f in food:
+		palgame.draw_circle(palgame.get_x(f), palgame.get_y(f), palgame.get_radius(f), BLUE)
+	for i, b in enumerate(balls):
+		if i == 0:
+			color = GREEN
+		else:
+			color = RED
+		palgame.draw_circle(palgame.get_x(b), palgame.get_y(b), palgame.get_radius(b), color)
+
+	palgame.draw_everything()
--- palgame.py
+++ palgame.py
@@ -1,6 +1,11 @@
 import pygame, sys
+import random
+import math
 BALL_COLOR = (255, 0, 0)
 SCREEN_COLOR = (100, 100, 100)
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 700
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -10,22 +15,66 @@ def build_screen(width, height):
 	global screen
 	screen = pygame.display.set_mode((width, height))
 
-def draw_circle(x, y, radius):
-	pygame.draw.circle(screen, BALL_COLOR, (x, y), radius)
-	pygame.display.flip()
+def draw_circle(x, y, radius, color):
+	pygame.draw.circle(screen, color, (x, y), radius)
 
 def draw_rect(x, y, width, height):
 	pygame.draw.rect(screen, BALL_COLOR, (x, y, width, height), 0)
-	pygame.display.flip()
 
 def clear_screen():
 	screen.fill(SCREEN_COLOR)
+
+def draw_everything():
+	pygame.display.flip()
 
 def get_event():
 	clock.tick(20)
 	for e in pygame.event.get():
 		if e.type == pygame.QUIT:
 			sys.exit(0)
+
+def get_x(ball):
+	return ball[0]
+
+def get_y(ball):
+	return ball[1]
+
+def set_x(ball, x):
+	ball[0] = x
+
+def set_y(ball, y):
+	ball[1] = y
+
+def get_radius(ball):
+	return ball[2]
+
+def set_radius(ball, r):
+	ball[2] = r
+
+def get_random_ball():
+	return [random.randint(0, SCREEN_WIDTH), 
+		random.randint(0, SCREEN_HEIGHT),
+		random.randint(6, 20),
+		1, -1]
+
+def get_random_food():
+	return [random.randint(0, SCREEN_WIDTH), 
+		random.randint(0, SCREEN_HEIGHT), 
+		5]
+
+def get_speed(radius):
+	return 1 + int(50 / (radius * 0.3))
+
+def get_vx(speed, mx, my, x, y):
+	if abs(mx - x) <= 1 or abs(my - y) <= 1:
+		return 0
+	return (speed * (mx - x) / math.sqrt((mx - x)**2 + (my - y)**2))
+
+def get_vy(speed, mx, my, x, y):
+	if abs(mx - x) <= 1 or abs(my - y) <= 1:
+		return 0
+	return (speed * (my - y) / math.sqrt((mx - x)**2 + (my - y)**2))
+
 
 # Returns the location of the mouse as (x, y). For example:
 # event = get_mouse_location()
