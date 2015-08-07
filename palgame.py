742f67c2f5c1a94f29c09306ba946c533d294383
Jessie Duan <jduan48@gmail.com>
2015-08-07 09:25:03 +0300

    added documentation for palgame

--- palgame.py
+++ palgame.py
@@ -13,28 +13,38 @@ pygame.init()
 clock = pygame.time.Clock()
 screen = None
 
+# This function creates the window for the game. It should be called at
+# the beginning of your program.
 def build_screen(width, height):
 	global screen
 	screen = pygame.display.set_mode((width, height))
 
+# This function draws a circle.
 def draw_circle(x, y, radius, color):
 	pygame.draw.circle(screen, color, (x, y), radius)
 
+# This function draws a rectangle.
 def draw_rect(x, y, width, height):
 	pygame.draw.rect(screen, BALL_COLOR, (x, y, width, height), 0)
 
+# This function wipes the screen clean by filling it with a color.
+# This lets you draw new objects, or make objects look like they've moved.
 def clear_screen():
 	screen.fill(SCREEN_COLOR)
 
+# This function actually puts all objects on the screen, after you've
+# drawn them with draw_circle and draw_rect.
 def draw_everything():
 	pygame.display.flip()
 
+# This function lets you exit the game if you press the "X" button.
 def get_event():
 	clock.tick(20)
 	for e in pygame.event.get():
 		if e.type == pygame.QUIT:
 			sys.exit(0)
 
+# These functions allow you to get and set x, y, radius, xdir, and ydir for a ball.
 def get_x(ball):
 	return ball[0]
 
@@ -65,22 +75,30 @@ def get_radius(ball):
 def set_radius(ball, r):
 	ball[2] = r
 
+# This function returns a ball with a random position and random radius.
+# We describe a ball as a list: ball[0] is x, ball[1] is y, ball[2] is radius,
+# ball[3] is xdir, and ball[4] is ydir.
 def get_random_ball():
 	return [random.randint(0, SCREEN_WIDTH), 
 		random.randint(0, SCREEN_HEIGHT),
 		random.randint(6, 20),
 		1, -1]
 
+# This function returns a ball to represent food, with a random position and
+# radius of 5. This ball does not have xdir or ydir because it doesn't move.
 def get_random_food():
 	return [random.randint(0, SCREEN_WIDTH), 
 		random.randint(0, SCREEN_HEIGHT), 
-		5]
+		5, 0, 0]
 
+# This function returns a ball to represent the user, with radius 3.
 def get_random_user():
 	ball = get_random_ball()
 	set_radius(ball, 3)
 	return ball
 
+# This function returns a speed, given a radius. If the radius is larger, the ball
+# moves slower. If the radius is smaller, the ball moves faster.
 def get_speed(radius):
 	return 1 + int(50 / (radius * 0.3))
 
