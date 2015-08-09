e955f63a2f1e4a376821450806c117ba96df97da
Arushi Jain <arushijain@Arushis-MacBook-Pro.local>
2015-08-09 15:50:08 +0300

    added commenting

--- starter_breakout.py
+++ starter_breakout.py
@@ -19,7 +19,9 @@ Paddle: Methods
 # Update the position of the paddle. It is confined to the boundaries
 # of the screen
 def paddle_update_position(paddle):
-    breakout.set_x(paddle, constants.clamp(breakout.get_x(paddle) + breakout.get_x_velocity(paddle), 0, constants.SCREEN_WIDTH - breakout.get_width(paddle)))
+    coordinates =  breakout.get_mouse_position()
+    x = coordinates[0]
+    breakout.set_x(paddle, constants.clamp(x + breakout.get_x_velocity(paddle), 0, constants.SCREEN_WIDTH - breakout.get_width(paddle)))
 
 """
 Ball: Methods
@@ -56,42 +58,42 @@ def ball_bounce_off_brick(ball, brick):
         breakout.set_x_velocity(ball, -x_v)
 
     # We hit the brick from the side so change x direction
-    #if breakout.get_x(ball) in range(breakout.get_x(brick), breakout.get_x(brick) + breakout.get_width(brick)):
+    # if breakout.get_x(ball) in range(breakout.get_x(brick), breakout.get_x(brick) + breakout.get_width(brick)):
     if breakout.get_y(ball) + breakout.get_radius(ball) >= breakout.get_y(brick) and breakout.get_y(ball) - breakout.get_radius(ball) < breakout.get_y(brick) + breakout.get_height(brick):
         y_v = breakout.get_y_velocity(ball)
         breakout.set_y_velocity(ball, -y_v)  
 
 
+# Check and see if the ball and another obj collided with each other 
 def ball_did_collide_with(ball, obj):
     if breakout.get_x(ball) + breakout.get_radius(ball) >= breakout.get_x(obj) and breakout.get_x(ball) - breakout.get_radius(ball) <= breakout.get_x(obj) + breakout.get_width(obj) and breakout.get_y(ball) + breakout.get_radius(ball) >= breakout.get_y(obj) and breakout.get_y(ball) - breakout.get_radius(ball) < breakout.get_y(obj) + breakout.get_height(obj):
         return True 
 
 
-
-def play(screen, paddle, ball, bricks, start, lives):
-    pygame.font.init()
-    font = pygame.font.Font(None, 36)
+def play(screen, paddle, ball, bricks, start):
     running = True
+    lives = constants.NUM_LIVES
     while running:
         if lives == 0:
             running = False
-            loseText = font.render("You Lose.", True, constants.WHITE)
-            screen.blit(loseText, (200, 200))
+
+        #Setup the keyboard events 
         for event in pygame.event.get():
+            # If you press the up key, the ball will start moving 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
                     start = True            
             if event.type == pygame.QUIT:
                 running = False
-        coordinates =  breakout.get_mouse_position()
-        x = coordinates[0]
-        breakout.set_x(paddle,  x)
 
+        # Only if start is True you want the ball to be moving. This boolean is used to keep the ball at the centre before the user hits the UP key 
         if start:
             ball_update_position(ball)
         
+        #Update the position of the paddle based on the mouse
         paddle_update_position(paddle)
         
+        #Check for collisions 
         if ball_did_collide_with(ball, paddle):
             ball_bounce_off_paddle(ball, paddle)
 
@@ -102,11 +104,11 @@ def play(screen, paddle, ball, bricks, start, lives):
             ball = breakout.create_new_ball()
             start = False
 
+        # If bricks are over, you won! 
         if len(bricks) == 0:
             running = False;
-            loseText = font.render("You Win.", True, constants.WHITE)
-            screen.blit(loseText, (200, 200))
 
+        # Else, loop through the entire bricks array to see if the ball collided with any brick 
         else:
             for brick in bricks:
                 if  ball_did_collide_with(ball, brick):
@@ -116,31 +118,38 @@ def play(screen, paddle, ball, bricks, start, lives):
 
         # Redraw everything at the end of the while loop
         breakout.draw_objects(screen, paddle, ball, bricks)
+
     pygame.display.update()
+
+    #Wait for event to exit screen
     event =  pygame.event.wait()
     if event.type == pygame.QUIT:
-        
         pygame.quit()
 
 
+# The following function will draw the set of bricks at the top of the screen. 
 def build_bricks():
+    #Create an empty array
     bricks = []
-    for i in range(constants.NUM_ROWS):
-        for j in range(constants.BRICKS_PER_ROW):
-            x_position = constants.GAP + (j * (constants.BRICK_WIDTH + constants.GAP))
-            y_position = i * (constants.BRICK_HEIGHT + constants.GAP)
+    for row in range(constants.NUM_ROWS):
+        for col in range(constants.BRICKS_PER_ROW):
+            x_position = constants.GAP + (col * (constants.BRICK_WIDTH + constants.GAP))
+            y_position = row* (constants.BRICK_HEIGHT + constants.GAP)
+
+            # Set the brick color based on row number 
             color = None
-            if (i == 0 or i == 1):
+            if (row == 0 or row == 1):
                 color = constants.RED
-            elif (i == 2 or i  == 3):
+            elif (row == 2 or row  == 3):
                 color = constants.ORANGE
-            elif (i  == 4 or i  == 5):
+            elif (row  == 4 or row  == 5):
                 color = constants.GREEN
-            elif (i == 6 or i  == 7):
-                color = constants.YELLOW
+            elif (row == 6 or row  == 7):
+                color = constants.YELLOcol
             else: 
                 color = constants.YELLOW
 
+            #Create a new brick and set the x,y, and color 
             b = breakout.create_new_brick()
             breakout.set_x(b, x_position)
             breakout.set_y(b, y_position)
@@ -150,20 +159,19 @@ def build_bricks():
 
 
 if __name__ == '__main__':
+
     #Ignore the following code
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     pygame.display.set_caption('Breakout')
-
-
-
-
     clock = pygame.time.Clock()
+
+    #Create the ball, paddle, bricks and lives here 
     lives = constants.NUM_LIVES
     paddle = breakout.create_new_paddle()
     ball = breakout.create_new_ball()
     bricks = build_bricks();
 
     start = False
-    play(screen, paddle, ball, bricks, start, lives)
+    play(screen, paddle, ball, bricks, start)
 
